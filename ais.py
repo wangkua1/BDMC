e46db73e6392f79f9f8be56981ac56c1532e6826
wangkua1 <wangkua1@cs.toronto.edu>
2020-10-23 14:24:51 -0400

    hmc sweep

--- ais.py
+++ ais.py
@@ -20,6 +20,8 @@ def ais_trajectory(model,
                    schedule=np.linspace(0., 1., 500),
                    n_sample=100,
                    log_likelihood_fn=utils.log_bernoulli,
+                   log_prior_fn=None,
+                   prior_sample_fn=None,
                    save_dir=None):
   """Compute annealed importance sampling trajectories for a batch of data. 
   Could be used for *both* forward and reverse chain in BDMC.
@@ -36,16 +38,17 @@ def ais_trajectory(model,
       A list where each element is a torch.autograd.Variable that contains the 
       log importance weights for a single batch of data
   """
-
+  if log_prior_fn is None: 
+    log_prior_fn = lambda z: utils.log_normal(z, torch.zeros_like(z), torch.zeros_like(z))
+  if prior_sample_fn is None:
+    prior_sample_fn = lambda B: torch.randn(B, model_latent_dim).cuda()
+  
   def log_f_i(z, data, t):
     """Unnormalized density for intermediate distribution `f_i`:
         f_i = p(z)^(1-t) p(x,z)^(t) = p(z) p(x|z)^t
     =>  log f_i = log p(z) + t * log p(x|z)
     """
-    with torch.no_grad():
-      # import ipdb; ipdb.set_trace()
-      zeros = torch.zeros(B, model_latent_dim).cuda()
-    log_prior = utils.log_normal(z, zeros, zeros)
+    log_prior = log_prior_fn(z)
     log_likelihood = log_likelihood_fn(model_decode_vector(z), data)
 
     return log_prior + log_likelihood.mul_(t)
@@ -57,21 +60,19 @@ def ais_trajectory(model,
     batch = utils.safe_repeat(batch, n_sample)
 
     with torch.no_grad():
-      # import  ipdb; ipdb.set_trace()
       epsilon = torch.ones(B).cuda().mul_(0.01)
       accept_hist = torch.zeros(B).cuda()
       logw = torch.zeros(B).cuda()
 
     # initial sample of z
     if forward:
-      current_z = torch.randn(B, model_latent_dim).cuda()
+      current_z = prior_sample_fn(B)
     else:
       current_z = utils.safe_repeat(post_z, n_sample).cuda()
     # current_z = current_z.requires_grad_()
     pbar = tqdm(enumerate(zip(schedule[:-1], schedule[1:]), 1))
     for j, (t0, t1) in pbar:
 
-      # import ipdb; ipdb.set_trace()
       current_z = current_z.detach()
       current_z.requires_grad_()
       # update log importance weight
@@ -111,26 +112,26 @@ def ais_trajectory(model,
           U, K=normalized_kinetic)
       tmp = utils.log_mean_exp(logw.view(n_sample, -1).transpose(0, 1)).mean().item()
       pbar.set_postfix_str(s=f'AIS: {tmp:.2f}', refresh=True)
-      # if j % 10 == 0: 
-      #   with torch.no_grad():
-      #     x = model_decode_vector(z)
-      #   h = w = int(np.sqrt(x.size(1)))
-      #   # Plot
-      #   def _plot(ims):
-      #     c, h, w = ims.shape[-3:]
-      #     grid = vutils.make_grid(ims.reshape(-1, c,h,w), nrow=8, padding=2, normalize=True) 
-      #     plt.imshow(np.transpose(grid.numpy(), (1,2,0)))
-      #     plt.tight_layout()
-      #     plt.grid()
-      #     plt.xticks([])
-      #     plt.yticks([])
-      #   f, axs = plt.subplots(1, 2, figsize=(8,5))
-      #   plt.subplot(axs[0])
-      #   _plot(x.view(x.size(0),1,h, w)[:64].cpu())
-      #   plt.subplot(axs[1])
-      #   _plot(batch.view(x.size(0),1,h, w)[:64].cpu())
-      #   plt.savefig(os.path.join(save_dir,f'{j}.jpeg'), bbox_inches='tight', pad_inches=0, format='jpeg')
-      #   # import ipdb;  ipdb.set_trace()
+      if j % 10 == 1: 
+        with torch.no_grad():
+          x = model_decode_vector(z)
+        h = w = int(np.sqrt(x.size(1)))
+        # Plot
+        def _plot(ims):
+          c, h, w = ims.shape[-3:]
+          grid = vutils.make_grid(ims.reshape(-1, c,h,w), nrow=8, padding=2, normalize=True) 
+          plt.imshow(np.transpose(grid.numpy(), (1,2,0)))
+          plt.tight_layout()
+          plt.grid()
+          plt.xticks([])
+          plt.yticks([])
+        f, axs = plt.subplots(1, 2, figsize=(8,5))
+        plt.subplot(axs[0])
+        _plot(x.view(x.size(0),1,h, w)[:64].cpu())
+        plt.subplot(axs[1])
+        _plot(batch.view(x.size(0),1,h, w)[:64].cpu())
+        plt.savefig(os.path.join(save_dir,f'{j}.jpeg'), bbox_inches='tight', pad_inches=0, format='jpeg')
+        
         
 
 
